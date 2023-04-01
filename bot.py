dd8f0af08c2bbe36f60a8c7db30a3e9b086f71ef
Daniil Kuznetsov <lfy050@yandex.ru>
2023-04-01 15:43:26 +0700

    тест 1

--- bot.py
+++ bot.py
@@ -2,28 +2,59 @@ import asyncio
 import json
 import os
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Update, Message
-from handlers import help_handler
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFahter
+TOKEN = "42:TOKEN"
+
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
 
 
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(text='Привет!')
+@router.message(Command(commands=["start"]))
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receive messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
 
-        
-async def main(update):
-    bot: Bot = Bot(token=os.environ.get('TOKEN'))
-    dp: Dispatcher = Dispatcher()
-    
-    dp.include_router(help_handler.router)
 
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward received message back to the sender
+
+    By default, message handler will handle all message types (like text, photo, sticker and etc.)
+    """
+    try:
+        # Send copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main(update) -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(os.environ.get('TOKEN'), parse_mode="HTML")
+    # And the run events dispatching
     await dp.feed_update(bot=bot, update=update)
 
 
 def starting_point(event, _):
-    update = Update.parse_obj(json.loads(event['body']))
+    update = types.Update.parse_obj(json.loads(event['body']))
     asyncio.run(main(update))
     return {
         'statusCode': 200,
